75ef05cf23206db07f1bfbc1fc10cec20518379d
Eivind Uggedal <eivind@uggedal.com>
2018-09-21 10:01:28 +0200

    todo

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@ pkg() {
 	brew ls --versions $1 >/dev/null || brew install $1
 }
 
+app() {
+	brew cask ls --versions $1 >/dev/null 2>/dev/null || brew cask install $1
+}
+
+pkg tmux
 pkg pass
 
 app firefox
