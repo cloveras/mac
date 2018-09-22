e84b13f2cf43ff4506178fd80e936f2c9b359a6b
Eivind Uggedal <eivind@uggedal.com>
2018-09-22 14:14:32 +0200

    mas

--- install.sh
+++ install.sh
@@ -8,7 +8,15 @@ app() {
 	brew cask ls --versions $1 >/dev/null 2>/dev/null || brew cask install $1
 }
 
+mas() {
+	command mas list | grep -q ^$1 || command mas install $1
+}
+
+pkg mas
+
 pkg tmux
 pkg pass
 
 app firefox
+
+mas 803453959  # Slack
